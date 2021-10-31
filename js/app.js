1577aa9f1f3b6dcd19198d156bc26d068c440b66
the-name-romel <rommelbon100@gmail.com>
2021-10-31 18:26:28 +0800

    Reading values from the Form

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
 //Variables
-
-
-
+const form = document.getElementById('request-quote');
 
 
 
 //Event Listeners
-document.addEventListener('DOMContentLoaded', function() {
-    //Create the options for the Years
-    const html = new HTMLUI() ;
-    html.displayYears();
+eventListeners();
 
-}); 
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', function() {
+        //Create the options for the Years
+        const html = new HTMLUI() ;
+        html.displayYears();
+    
+    }); 
+    
+    //When the form is submitted
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
 
+         //Read the values from the form
+    const make = document.getElementById('make').value;
+    const year = document.getElementById('year').value;
+    //Read the radio buttons
+    const level = document.querySelector('input[name="level"]:checked').value;
+
+    //Check that all the fields have something
+    if( make === '' || year === '' || level === '') {
+        console.log(error);
+
+    } else {
+        console.log('Allright!');
+    }
+
+    });
+ 
+}
 
 
 //Objects
@@ -24,7 +46,7 @@ HTMLUI.prototype.displayYears = function() {
     //Max and Minimum Years
     const max = new Date().getFullYear(),
           min = max - 20;
-console.log(min);
+
     //Generate the list with the 20 years
     const selectYears = document.getElementById('year');
 
